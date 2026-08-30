c1f6579cfdad386f5b71cfbac990c05606cdbe8e
Saad Malik <saadmali@gensosekai.com>
2026-08-29 17:45:51 -0700

    switching to custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://saadimalik211.github.io",
-  base: "/resumeWebsite/",
+  site: "https://resume.gensosekai.com",
+  base: "/",
   output: "static",
 });
